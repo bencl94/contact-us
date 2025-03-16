44dc5fb45710b7117b58280d31819b2c377f122d
Benjamin Clauß <90100610+bencl94@users.noreply.github.com>
2025-03-16 18:27:02 +0100

    Finish desktop version

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function openNavbar() {
-    alert('Under construction');
+function toggleNavbar() {
+    const nav = document.getElementById('nav-menu');
+    const hamburger = document.getElementById('hamburger-icon');
+    hamburger.classList.toggle('hamburger-active')
+
+    if (nav.style.display == 'none') {
+        nav.style.display = 'block';
+    } else {
+        nav.style.display == 'none'
+    }
+
 }
